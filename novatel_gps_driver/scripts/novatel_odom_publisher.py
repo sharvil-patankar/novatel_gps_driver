331e9e8f4a7e98dd830c36378137700fb3863882
hirokawakubo <hiro.kawakubo@bordrin.com>
2019-10-25 18:04:31 -0400

    Switched from Inscov to Insstdev

--- novatel_gps_driver/scripts/novatel_odom_publisher.py
+++ novatel_gps_driver/scripts/novatel_odom_publisher.py
@@ -5,7 +5,7 @@ import numpy as np
 import tf2_ros
 from geometry_msgs.msg import TransformStamped
 from nav_msgs.msg import Odometry
-from novatel_gps_msgs.msg import NovatelUtmPosition, NovatelVelocity, Inscov
+from novatel_gps_msgs.msg import NovatelUtmPosition, NovatelVelocity, Insstdev
 from sensor_msgs.msg import Imu
 from tf.transformations import quaternion_multiply, quaternion_conjugate
 import message_filters
@@ -16,26 +16,26 @@ class NovatelOdomPublisher:
         self.rate = rospy.Rate(10)
         self.pub_odom = rospy.Publisher('novatel/odom', Odometry, queue_size=10)
         self.utm_sub = message_filters.Subscriber('novatel/bestutm', NovatelUtmPosition)
-        self.inscov_sub = message_filters.Subscriber('novatel/inscov', Inscov)
+        self.insstdev_sub = message_filters.Subscriber('novatel/insstdev', Insstdev)
         self.vel_sub = message_filters.Subscriber('novatel/bestvel', NovatelVelocity)
         self.imu_sub = message_filters.Subscriber('novatel/imu', Imu)
 
         self.odom = Odometry()
         self.odom.header.frame_id = 'map'
-        self.odom.child_frame_id = 'gps_imu'
+        self.odom.child_frame_id = 'base_link'
         
         self.br = tf2_ros.TransformBroadcaster()
         self.t = TransformStamped()
         self.t.header.frame_id = 'map'
-        self.t.child_frame_id = 'gps_imu'
+        self.t.child_frame_id = 'base_link'
 
     def sub_and_pub(self):
         self.ts = message_filters.ApproximateTimeSynchronizer([
-            self.utm_sub, self.inscov_sub, self.vel_sub, self.imu_sub], 10, 0.01)
+            self.utm_sub, self.insstdev_sub, self.vel_sub, self.imu_sub], 10, 0.01)
         self.ts.registerCallback(self.publish_odom)
         rospy.spin()
 
-    def publish_odom(self, utm, inscov, vel, imu):
+    def publish_odom(self, utm, insstdev, vel, imu):
         self.odom.header.stamp = rospy.Time.now()
         self.odom.pose.pose.position.x = utm.easting
         self.odom.pose.pose.position.y = utm.northing
@@ -57,9 +57,10 @@ class NovatelOdomPublisher:
         self.odom.twist.twist.linear.x = self.body_v[0]
         self.odom.twist.twist.linear.y = self.body_v[1]
         self.odom.twist.twist.linear.z = self.body_v[2]
+	self.lin_vel_cov = np.diag(np.array([ insstdev.east_velocity_dev**2, insstdev.north_velocity_dev**2, insstdev.up_velocity_dev**2]))
         self.odom.twist.covariance = ((np.bmat([
-            [np.reshape(inscov.velocity_covariance, (3, 3)), np.zeros([3, 3])                                  ],
-            [np.zeros([3, 3]),                               np.reshape(imu.angular_velocity_covariance, (3, 3))]
+            [ self.lin_vel_cov, np.zeros([3, 3])                                  ],
+            [ np.zeros([3, 3]), np.reshape(imu.angular_velocity_covariance, (3, 3))]
                                                 ])).A.flatten()).tolist()
         self.pub_odom.publish(self.odom)
 
