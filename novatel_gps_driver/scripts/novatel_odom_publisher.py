7ef05b79b683b06d645e3b8d9abf95293bb0e04f
sharvil-patankar <sharvil.patankar@bordrin.com>
2019-10-30 17:18:37 -0400

    Updated transform

--- novatel_gps_driver/scripts/novatel_odom_publisher.py
+++ novatel_gps_driver/scripts/novatel_odom_publisher.py
@@ -3,7 +3,7 @@
 import rospy
 import numpy as np
 import tf2_ros
-from geometry_msgs.msg import TransformStamped
+from geometry_msgs.msg import TransformStamped, PoseStamped
 from nav_msgs.msg import Odometry
 from novatel_gps_msgs.msg import NovatelUtmPosition, NovatelVelocity, Insstdev
 from sensor_msgs.msg import Imu
@@ -22,17 +22,18 @@ class NovatelOdomPublisher:
 
         self.odom = Odometry()
         self.odom.header.frame_id = 'map'
-        self.odom.child_frame_id = 'gps_imu'
+        self.odom.child_frame_id = 'base_link'
 
         self.buff = tf2_ros.Buffer()
         self.lis = tf2_ros.TransformListener(self.buff)
         # self.trans = self.buff.lookup_transform('gps_imu','base_footprint', rospy.Time.now(), rospy.Duration(1.0))
         # self.trans.transform()
         self.lis.unregister()
+        self.temp_pose = PoseStamped
         self.br = tf2_ros.TransformBroadcaster()
-        self.t_temp = TransformStamped()
-        self.t_temp.header.frame_id = 'map'
-        self.t_temp.child_frame_id = 'gps_imu'
+        self.t = TransformStamped()
+        self.t.header.frame_id = 'map'
+        self.t.child_frame_id = 'bese_footprint'
 
     def sub_and_pub(self):
         self.ts = message_filters.ApproximateTimeSynchronizer([
@@ -52,7 +53,15 @@ class NovatelOdomPublisher:
             [self.position_covariance, np.zeros([3, 3])                              ],
             [np.zeros([3, 3]),         np.reshape(imu.orientation_covariance, (3, 3))]
                                                ])).A.flatten()).tolist()
+
+        self.temp_pose.header = self.odom.header
+        self.temp_pose.pose = self.odom.pose.pose
+        self.temp_pose = self.buff.transform(self.temp_pose,'base_footprint')
+        self.odom.header = self.temp_pose.header
+        self.odom.pose = self.temp_pose.pose
+
         self.odom.twist.twist.angular = imu.angular_velocity
+
         self.pure_v = [np.cos(np.radians(vel.track_ground)) * vel.horizontal_speed,
                        np.sin(np.radians(vel.track_ground)) * vel.horizontal_speed,
                        vel.vertical_speed,
@@ -69,10 +78,9 @@ class NovatelOdomPublisher:
                                                 ])).A.flatten()).tolist()
         self.pub_odom.publish(self.odom)
 
-        self.t_temp.header.stamp = self.odom.header.stamp
-        self.t_temp.transform.translation = self.odom.pose.pose.position
-        self.t_temp.transform.rotation = self.odom.pose.pose.orientation
-        self.t = self.buff.transform(self.t,'base_footprint')
+        self.t.header.stamp = self.odom.header.stamp
+        self.t.transform.translation = self.odom.pose.pose.position
+        self.t.transfrom.rotation = self.odom.pose.pose.orientation
         self.br.sendTransform(self.t)
 
         self.rate.sleep()
