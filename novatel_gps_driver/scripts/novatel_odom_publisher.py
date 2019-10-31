0e2946dac4fca74d9925d67e669ea644b1ce8e27
sharvil-patankar <sharvil.patankar@bordrin.com>
2019-10-31 10:36:55 -0400

    Made odom into a local frame and added the option to change the local origin

--- novatel_gps_driver/scripts/novatel_odom_publisher.py
+++ novatel_gps_driver/scripts/novatel_odom_publisher.py
@@ -3,8 +3,9 @@
 import rospy
 import numpy as np
 import tf2_ros
-from tf2_geometry_msgs.tf2_geometry_msgs import PoseStamped
-from geometry_msgs.msg import TransformStamped
+# from tf2_geometry_msgs.tf2_geometry_msgs import PoseStamped
+# import tf2_geometry_msgs.tf2_geometry_msgs as tf2_gm
+from geometry_msgs.msg import TransformStamped, Point
 from nav_msgs.msg import Odometry
 from novatel_gps_msgs.msg import NovatelUtmPosition, NovatelVelocity, Insstdev
 from sensor_msgs.msg import Imu
@@ -22,71 +23,63 @@ class NovatelOdomPublisher:
         self.imu_sub = message_filters.Subscriber('novatel/imu', Imu)
 
         self.odom = Odometry()
-        self.odom.header.frame_id = 'map'
+        self.odom.header.frame_id = 'odom'
         self.odom.child_frame_id = 'base_link'
 
         self.buff = tf2_ros.Buffer()
         self.lis = tf2_ros.TransformListener(self.buff)
-        # self.trans = self.buff.lookup_transform('gps_imu','base_footprint', rospy.Time.now(), rospy.Duration(1.0))
-        # self.trans.transform()
         self.lis.unregister()
-        self.temp_pose = PoseStamped
         self.br = tf2_ros.TransformBroadcaster()
         self.t = TransformStamped()
-        self.t.header.frame_id = 'map'
-        self.t.child_frame_id = 'bese_footprint'
+        self.t.header.frame_id = 'odom'
+        self.t.child_frame_id = 'base_link'
 
-    def sub_and_pub(self):
+    def sub_and_pub(self, origin):
+        self.origin = Point()
+        self.origin.x = origin['x']
+        self.origin.y = origin['y']
+        self.origin.z = origin['z']
         self.ts = message_filters.ApproximateTimeSynchronizer([
             self.utm_sub, self.insstdev_sub, self.vel_sub, self.imu_sub], 10, 0.01)
-        rospy.loginfo("time synchronized")
-	self.ts.registerCallback(self.publish_odom)
+        self.ts.registerCallback(self.publish_odom)
         rospy.spin()
 
     def publish_odom(self, utm, insstdev, vel, imu):
-        rospy.loginfo("In callback")
-	self.odom.header.stamp = rospy.Time.now()
-        self.odom.pose.pose.position.x = utm.easting
-        self.odom.pose.pose.position.y = utm.northing
-        self.odom.pose.pose.position.z = utm.height
+        self.odom.header.stamp = rospy.Time.now()
+        self.odom.pose.pose.position.x = utm.easting - self.origin.x
+        self.odom.pose.pose.position.y = utm.northing - self.origin.y
+        self.odom.pose.pose.position.z = utm.height - self.origin.z
         self.odom.pose.pose.orientation = imu.orientation
 
         self.position_covariance = np.diag([utm.easting_sigma**2, utm.northing_sigma**2, utm.height_sigma**2])
         self.odom.pose.covariance = ((np.bmat([
             [self.position_covariance, np.zeros([3, 3])                              ],
             [np.zeros([3, 3]),         np.reshape(imu.orientation_covariance, (3, 3))]
-                                               ])).A.flatten()).tolist()
-
-        self.temp_pose.header = self.odom.header
-        self.temp_pose.pose = self.odom.pose.pose
-        self.temp_pose = self.buff.transform(self.temp_pose,'base_footprint')
-        self.odom.header = self.temp_pose.header
-        self.odom.pose = self.temp_pose.pose
+                                              ])).A.flatten()).tolist()
 
         self.odom.twist.twist.angular = imu.angular_velocity
 
         self.pure_v = [np.cos(np.radians(vel.track_ground)) * vel.horizontal_speed,
                        np.sin(np.radians(vel.track_ground)) * vel.horizontal_speed,
                        vel.vertical_speed,
-                       0                                                      ]
+                       0                                                          ]
         self.q = [imu.orientation.x, imu.orientation.y, imu.orientation.z, imu.orientation.w]
         self.body_v = quaternion_multiply(quaternion_multiply(quaternion_conjugate(self.q), self.pure_v), self.q)
         self.odom.twist.twist.linear.x = self.body_v[0]
         self.odom.twist.twist.linear.y = self.body_v[1]
         self.odom.twist.twist.linear.z = self.body_v[2]
-	self.lin_vel_cov = np.diag(np.array([ insstdev.east_velocity_dev**2, insstdev.north_velocity_dev**2, insstdev.up_velocity_dev**2]))
+        self.lin_vel_cov = np.diag(np.array([insstdev.east_velocity_dev**2,
+                                             insstdev.north_velocity_dev**2,
+                                             insstdev.up_velocity_dev**2]))
         self.odom.twist.covariance = ((np.bmat([
-            [ self.lin_vel_cov, np.zeros([3, 3])                                  ],
-            [ np.zeros([3, 3]), np.reshape(imu.angular_velocity_covariance, (3, 3))]
-                                                ])).A.flatten()).tolist()
+            [self.lin_vel_cov, np.zeros([3, 3])                                  ],
+            [np.zeros([3, 3]), np.reshape(imu.angular_velocity_covariance, (3, 3))]
+            ])).A.flatten()).tolist()
         self.pub_odom.publish(self.odom)
 
-        self.t_temp.header.stamp = self.odom.header.stamp
-        self.t_temp.transform.translation = self.odom.pose.pose.position
-        self.t_temp.transform.rotation = self.odom.pose.pose.orientation
-        self.t = self.buff.transform(self.t_temp,'base_footprint')
+        self.t.transform.translation = self.odom.pose.pose.position
+        self.t.transform.rotation = self.odom.pose.pose.orientation
         self.br.sendTransform(self.t)
-
         self.rate.sleep()
 
 
@@ -94,6 +87,7 @@ if __name__ == '__main__':
     rospy.init_node('novatel_odom_publisher', anonymous=True)
     try:
         novatel_odom_publisher_class = NovatelOdomPublisher()
-        novatel_odom_publisher_class.sub_and_pub()
+        odom_origin = rospy.get_param("origin")
+        novatel_odom_publisher_class.sub_and_pub(odom_origin)
     except rospy.ROSInterruptException:
         pass
